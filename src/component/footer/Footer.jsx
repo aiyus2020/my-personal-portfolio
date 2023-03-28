efc30e33af1c2e96fc74fda7be7d0a92c393a8e7
aiyus2020 <aiyusboss@gmail.com>
2023-03-28 09:26:45 +0100

    footer section

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { RiFacebookFill } from "react-icons/ri";
+import { FiInstagram } from "react-icons/fi";
+import { BsTwitter } from "react-icons/bs";
+
 const Footer = () => {
   return (
-    <section>
-      footer
-    </section>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        AIYUS
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <RiFacebookFill />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <BsTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; AIYUS TEch. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
