1cedfdefda374c1fa63818d5d73a772921fcbd63
aiyus2020 <aiyusboss@gmail.com>
2023-03-29 17:23:04 +0100

    img

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import HeaderSocials from './HeaderSocials'
-import ME from '../../assets/me.png'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/my img.jpg";
 const Header = () => {
   return (
-    
-  <header id='#'>
-    <div className="container header__container">
-      <h5>Hello I'am</h5>
-      <h1>aiyus</h1>
-      <h5 className='text-light'>
-        Fullstack Developer
-      </h5>
-      <CTA/>
-      <HeaderSocials/>
-      <div className="me">
-        <img src={ME} alt="me" />
+    <header id="#">
+      <div className="container header__container">
+        <h5>Hello I'am</h5>
+        <h1>aiyus</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll_Down
+        </a>
       </div>
-      <a href="#contact" className='scroll__down'>Scroll_Down</a>
-      
-    </div>
-  </header>
-   
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
